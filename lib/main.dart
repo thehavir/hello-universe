74e33f0b03efaca20945f0fd0c890110d195d005
Havir <havir@posteo.net>
2021-04-02 22:18:30 +0430

    Image details page ui is implemented

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,19 @@ import 'package:hello_universe/features/image_detail/view/image_details_page.dar
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello Universe!',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ImageDetailsPage(
+        title: 'Image details',
+      ),
+    );
+  }
+}
