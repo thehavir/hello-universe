60211e7ccff2c3c128a73b4d3df5d9fc0d3d3169
Havir <havir@posteo.net>
2021-05-07 23:27:04 +0430

    Image list cubit logic is implemented

--- lib/repository/nasa_apod_repository.dart
+++ lib/repository/nasa_apod_repository.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:hello_universe/api/models/apod.dart';
 
 /// Contract for the Nasa Apod repository.
@@ -7,4 +8,11 @@ import 'package:hello_universe/api/models/apod.dart';
 abstract class NasaApodRepository {
   /// Abstract method for fetching the picture of day from Nasa APOD repository.
   Future<PictureOfDay> fetchPictureOfDay();
+
+  /// Abstract method for fetching list of images (APOD). The list is contains
+  /// images from [startDate] to [endDate].
+  Future<List<PictureOfDay>> fetchImageList({
+    @required String startDate,
+    @required String endDate,
+  });
 }
