0781139bd551fcdb1b612610edb9c6d027355aa8
Havir <75127518+thehavir@users.noreply.github.com>
2022-01-09 21:16:09 +0330

    Add DateExtension test (#21)

---
+++ test/features/image_list/extensions/date_extension_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hello_universe/features/image_list/extensions/extension.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  group('Test `DateExtension`', () {
+    final DateTime now = DateTime.now();
+
+    test('Test format method', () {
+      final DateFormat formatter = DateFormat('yyyy-MM-dd');
+
+      expect(now.format(), formatter.format(now));
+    });
+
+    test('Test xDaysBefore method', () {
+      expect(now.xDaysBefore(1), now.subtract(Duration(days: 1)));
+      expect(now.xDaysBefore(19), now.subtract(Duration(days: 19)));
+    });
+  });
+}
